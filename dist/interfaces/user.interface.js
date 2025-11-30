61363fc8cd84f91fe9634415437c826b0c3e674d
ArnabShikder24 <arnabshikder2001@gmail.com>
2025-11-30 22:00:14 +0600

    dist

---
+++ dist/interfaces/user.interface.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Role = void 0;
+var Role;
+(function (Role) {
+    Role["admin"] = "admin";
+    Role["moderator"] = "moderator";
+    Role["member"] = "member";
+})(Role || (exports.Role = Role = {}));
